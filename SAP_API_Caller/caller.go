a46865d530d3db7c0fed0f2a2740c30b67c745ee
Yurino Oga <yurino.o@latona.io>
2022-09-01 13:25:56 +0900

    release

--- SAP_API_Caller/caller.go
+++ SAP_API_Caller/caller.go
@@ -35,7 +35,7 @@ func (c *SAPAPICaller) AsyncPostXXXXXXXX(
 	item              *requests.Item,
 	accepter []string) {
 	wg := &sync.WaitGroup{}
-	wg.Add(1)
+	wg.Add(len(accepter))
 	for _, fn := range accepter {
 		switch fn {
 		case "Header":
